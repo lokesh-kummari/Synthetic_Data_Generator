cacba6724d35f396be33ebbfbb61b4c59dc7a51b
lokesh <lokesh@MacBook-Air-2.local>
2025-03-23 15:43:54 +0530

    env req4

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
